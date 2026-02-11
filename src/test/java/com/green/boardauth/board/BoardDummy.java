c6284750ef16ae7f44f5f3d9b249cfab9df26478
kgee0503 <kgee0503@gmail.com>
2026-02-11 23:24:52 +0900

    Board Dummy 완료

--- src/test/java/com/green/boardauth/board/BoardDummy.java
+++ src/test/java/com/green/boardauth/board/BoardDummy.java
@@ -2,7 +2,7 @@ package com.green.boardauth.board;
 
 import com.green.boardauth.Dummy;
 import com.green.boardauth.application.board.BoardMapper;
-import com.green.boardauth.application.board.model.BoardPostReq;
+import com.green.boardauth.application.board.model.BoardPostPutReq;
 import org.apache.ibatis.session.ExecutorType;
 import org.apache.ibatis.session.SqlSession;
 import org.junit.jupiter.api.Test;
@@ -13,25 +13,28 @@ public class BoardDummy extends Dummy {
 
     @Test
     void generate() {
+
         SqlSession sqlSession = sqlSessionFactory.openSession(ExecutorType.BATCH);
         BoardMapper boardMapper = sqlSession.getMapper(BoardMapper.class);
+        try {
+            for (int i = 0; i < ADD_ROW_COUNT; i++) {
+                String title = koFaker.book().title();
+                String contents = koFaker.lorem().sentence(30);
+                long userId = koFaker.random().nextInt(4, 1003);
 
-        for (int i = 0; i < ADD_ROW_COUNT; i++) {
-            String title = koFaker.book().title();
-            String content = koFaker.lorem().sentence();
-            long userId = koFaker.random().nextInt(4, 1003);
+                System.out.printf("title: %s\n", title);
+                System.out.printf("contents: %s\n", contents);
+                System.out.printf("userId: %d\n", userId);
 
-            System.out.printf("title: %s\n", title);
-            System.out.printf("content: %s\n", content);
-            System.out.printf("userId: %s\n", userId);
+                BoardPostPutReq req = new BoardPostPutReq();
+                req.setTitle(title);
+                req.setContents(contents);
+                req.setUserId(userId);
+                boardMapper.save(req);
+            }
+            sqlSession.flushStatements();
+        } catch (Exception e) {
 
-            BoardPostReq req = new BoardPostReq();
-            req.setTitle(title);
-            req.setContents(content);
-            req.setUserId(userId);
-
-            boardMapper.postBoard(req);
         }
-        sqlSession.flushStatements();
     }
 }
